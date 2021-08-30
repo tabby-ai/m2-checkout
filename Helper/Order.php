34e20e7277c846ca24baf6b2d4649f4b09d6aaf4
Sergey Balitsky <sergey.balitsky@gmail.com>
2021-08-30 15:58:02 +0300

    Get back failed orders action setting

--- Helper/Order.php
+++ Helper/Order.php
@@ -190,7 +190,7 @@ class Order extends \Magento\Framework\App\Helper\AbstractHelper
             $this->restoreQuote();
 
             // delete order if needed
-            //if ($this->_config->getValue('order_action_failed_payment') == 'delete') {
+            if ($this->_config->getValue('order_action_failed_payment') == 'delete') {
                 if ($this->_registry->registry('isSecureArea')) {
                     $this->_orderRepository->delete($order);
                 } else {
@@ -198,7 +198,7 @@ class Order extends \Magento\Framework\App\Helper\AbstractHelper
                     $this->_orderRepository->delete($order);
                     $this->_registry->unregister('isSecureArea');
                 }
-            //}
+            }
 
             return true;
         }
