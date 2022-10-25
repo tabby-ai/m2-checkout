076c636205aea6603c7543d9813e1dd5a5677ab6
Sergey Balitsky <sergey.balitsky@gmail.com>
2022-10-25 10:55:18 +0300

    Add php 7.0 compatibility

--- Controller/CsrfCompatibility.php
+++ Controller/CsrfCompatibility.php
@@ -4,31 +4,9 @@ namespace Tabby\Checkout\Controller;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\CsrfAwareActionInterface;
-use Magento\Framework\App\Request\InvalidRequestException;
-use Magento\Framework\App\RequestInterface;
 
 if (interface_exists(CsrfAwareActionInterface::class)) {
-    abstract class CsrfCompatibility extends Action implements CsrfAwareActionInterface
-    {
-        /**
-         * @param RequestInterface $request
-         * @return InvalidRequestException|null
-         */
-        public function createCsrfValidationException(
-            RequestInterface $request
-        ): ?InvalidRequestException {
-            return null;
-        }
-
-        /**
-         * @param RequestInterface $request
-         * @return bool|null
-         */
-        public function validateForCsrf(RequestInterface $request): ?bool
-        {
-            return true;
-        }
-    }
+    include __DIR__ . '/CsrfCompatibilityNew.php';
 } else {
     abstract class CsrfCompatibility extends Action
     {
