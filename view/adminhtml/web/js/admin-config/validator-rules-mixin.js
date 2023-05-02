a3c948b8389c464ecc98efca60e1c64156760ccb
Sergey Balitsky <sergey.balitsky@tabby.ai>
2023-05-02 18:44:44 +0300

    Fix admin key validation

--- view/adminhtml/web/js/admin-config/validator-rules-mixin.js
+++ view/adminhtml/web/js/admin-config/validator-rules-mixin.js
@@ -7,14 +7,14 @@ define([
         $.validator.addMethod(
             'validate-tabby-public-key',
             function (value) {
-                return utils.isEmptyNoTrim(value) || /^pk_(test_)?[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}$/.test(value);
+                return value === '******' || utils.isEmptyNoTrim(value) || /^pk_(test_)?[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}$/.test(value);
             },
             $.mage.__('Wrong PUBLIC key format. Must be <b>pk_[test_]xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</b>.')
         );
         $.validator.addMethod(
             'validate-tabby-secret-key',
             function (value) {
-                return utils.isEmptyNoTrim(value) || /^sk_(test_)?[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}$/.test(value);
+                return value === '******' || utils.isEmptyNoTrim(value) || /^sk_(test_)?[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}$/.test(value);
             },
             $.mage.__('Wrong SECRET key format. Must be <b>sk_[test_]xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</b>.')
         );
